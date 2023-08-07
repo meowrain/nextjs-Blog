fde890410791917b5d06d5192415adb4e52b3082
MeowRain <meowrain@126.com>
2023-08-07 15:01:01 +0800

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,7 @@ import Posts from "@/app/components/Posts";
 export default function Home() {
     return (
         <main className="px-6 mx-auto">
-            <p className="mt-12 mb-12 text-2xl text-center dark:text-white">
-                Hello and Welcome 👋,I'm MeowRain
-            </p>
+            <p className="mt-12 mb-12 text-2xl text-center dark:text-white"> Hello and Welcome👋,I'm MeowRain</p>
             <Posts/>
         </main>
     )
